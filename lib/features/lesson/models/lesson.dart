21fbc655e6f1a11a056ab79ba2af1eb3a64df9a6
root <root@localhost.localdomain>
2026-07-15 23:34:21 +0400

    Fix Lesson model

--- lib/features/lesson/models/lesson.dart
+++ lib/features/lesson/models/lesson.dart
@@ -1,11 +1,11 @@
 class Lesson {
   final int id;
   final String title;
-  final String content;
+  final String description;
 
   const Lesson({
     required this.id,
     required this.title,
-    required this.content,
+    required this.description,
   });
 }
