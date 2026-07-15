984e416f0e3b5358a37e9c588ef25db8f9a22244
root <root@localhost.localdomain>
2026-07-15 23:03:45 +0400

    Add lesson id field

--- lib/features/lesson/models/lesson.dart
+++ lib/features/lesson/models/lesson.dart
@@ -1,8 +1,10 @@
 class Lesson {
+  final int id;
   final String title;
   final String content;
 
   const Lesson({
+    required this.id,
     required this.title,
     required this.content,
   });
