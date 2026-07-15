dc267df9ea7246b92aef674489a64610b7079a57
root <root@localhost.localdomain>
2026-07-15 21:56:44 +0400

    Add lesson screen

---
+++ lib/features/lesson/screens/lesson_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../models/lesson.dart';
+
+class LessonScreen extends StatelessWidget {
+  final Lesson lesson;
+
+  const LessonScreen({
+    super.key,
+    required this.lesson,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(lesson.title),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          lesson.content,
+          style: const TextStyle(
+            fontSize: 18,
+            height: 1.7,
+          ),
+        ),
+      ),
+    );
+  }
+}
