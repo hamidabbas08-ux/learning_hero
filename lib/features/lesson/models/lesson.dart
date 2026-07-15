ebf7db678288b44a9fe923ca486fdbffbea601e0
root <root@localhost.localdomain>
2026-07-15 22:48:53 +0400

    Add lesson content field

--- lib/features/lesson/models/lesson.dart
+++ lib/features/lesson/models/lesson.dart
@@ -1,27 +1,9 @@
 class Lesson {
-  final int id;
   final String title;
-  final String description;
-  final bool completed;
+  final String content;
 
   const Lesson({
-    required this.id,
     required this.title,
-    required this.description,
-    this.completed = false,
+    required this.content,
   });
-
-  Lesson copyWith({
-    int? id,
-    String? title,
-    String? description,
-    bool? completed,
-  }) {
-    return Lesson(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      description: description ?? this.description,
-      completed: completed ?? this.completed,
-    );
-  }
 }
